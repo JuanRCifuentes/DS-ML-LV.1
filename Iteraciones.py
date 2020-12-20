7f6b6787267b3f24a1296805f05055aa24cf2b91
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2020-12-19 19:43:44 -0500

    first editions

---
+++ Iteraciones.py
@@ -0,0 +1,34 @@
+#while
+index = 0
+while index < 10:
+    print(index)
+    index += 1
+    if index == 5:
+        break
+
+#For
+frutas = ['manzana', 'banano', 'naranja']
+for fruta in frutas:
+    print(fruta)
+
+#iter()
+colores = ['azul', 'amarillo', 'rojo']
+iterador = iter(colores)
+print(next(iterador))
+print(next(iterador))
+print(next(iterador))
+
+#Using Dictionaries
+estudiantes = {
+    'mexico': 10,
+    'colombia': 15,
+    'puerto_rico': 4
+}
+for pais in estudiantes:
+    print(pais)
+for pais in estudiantes.keys():
+    print(pais)
+for numeros in estudiantes.values():
+    print(numeros)
+for items in estudiantes.items():
+    print(items)
---
+++ objetos_float.py
@@ -0,0 +1,11 @@
+x = 0.0
+for i in range(8):
+    x += 0.1
+
+print(x)
+
+cadena = f'Hola {str(x)}'
+cadena2 = "Hola " + str(x)
+print(cadena)
+print(cadena2)
+
