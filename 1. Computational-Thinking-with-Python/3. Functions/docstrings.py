2611a868f0819a400b5f2db0dec930e2ac5c5273
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2020-12-20 19:50:12 -0500

    fix

--- 1. Computational-Thinking-with-Python/3. Functions/docstrings.py
+++ 1. Computational-Thinking-with-Python/3. Functions/docstrings.py
@@ -1,9 +1,9 @@
 def suma(a, b):
     """Sums two values a y b
 
-    param int a any int
-    param int b any int
-    returns sum of a plus b
+        param int a any int
+        param int b any int
+        returns sum of a plus b
     """
     total = a + b
     return total
