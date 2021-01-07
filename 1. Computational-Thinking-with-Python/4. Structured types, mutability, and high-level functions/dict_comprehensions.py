74e4af15fe0c4eb2b5ef45c69c5dee18231ccc3a
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2021-01-07 05:28:18 -0500

    dictionaries comprehension

--- 1. Computational-Thinking-with-Python/4. Structured types, mutability, and high-level functions/dict_comprehensions.py
+++ 1. Computational-Thinking-with-Python/4. Structured types, mutability, and high-level functions/dict_comprehensions.py
@@ -1,4 +1,24 @@
+#With keys
 dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
-# Double each value in the dictionary
-double_dict1 = {k:v*2 for (k,v) in dict1.items()}
-print(double_dict1)
+dict1_keys = [k for k in dict1.keys()]
+print(dict1_keys)
+
+#With values
+dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+dict1_values = [k for k in dict1.values()]
+print(dict1_values)
+
+#With items
+dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+dict1_items = {llaves:valores for (llaves, valores) in dict1.items()}
+print(dict1_items)
+
+#Now we deal with a condition
+dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+dict1_items = {llave:valor for (llave, valor) in dict1.items() if valor > 2}
+print(dict1_items)
+
+#Testing...
+dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+dict1_keysfun = {k:k*2 for k in dict1.keys()}
+print(dict1_keysfun)
