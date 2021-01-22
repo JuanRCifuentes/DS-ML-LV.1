37adb11db3a04b6e0c8226cc3315ce9d92107e95
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2021-01-22 13:52:08 -0500

    updates

---
+++ 2. OOP and Algorithms with Python/classes.py
@@ -0,0 +1,22 @@
+class Hotel:
+    
+    def __init__(self, numero_maximo_de_huespedes, lugares_de_estacionamiento):
+        self.numero_maximo_de_huespedes = numero_maximo_de_huespedes
+        self.lugares_de_estacionamiento = lugares_de_estacionamiento
+        self.huespedes = 0
+
+    def anadir_huespedes(self, cantidad_de_huespedes):
+        self.huespedes += cantidad_de_huespedes
+
+    def checkout(self, cantidad_de_huespedes):
+        self.huespedes -= cantidad_de_huespedes
+
+    def ocupacion_total(self):
+        return self.huespedes
+
+
+hotel = Hotel(50, lugares_de_estacionamiento=20)
+print(hotel.lugares_de_estacionamiento)
+hotel.anadir_huespedes(3)
+hotel.checkout(1)
+print(hotel.ocupacion_total())
