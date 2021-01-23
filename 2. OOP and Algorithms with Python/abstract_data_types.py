cfd78fe3b9be3fe4cb967aebde3a4dba0fdcbd0f
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2021-01-23 09:54:15 -0500

    decorators, abstraction and decomposition

---
+++ 2. OOP and Algorithms with Python/abstract_data_types.py
@@ -0,0 +1,21 @@
+#Definition
+class Coordenada:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, otra_coordenada):
+        x_diff = (self.x - otra_coordenada.x)**2
+        y_diff = (self.y - otra_coordenada.y)**2
+
+        return (x_diff + y_diff)**0.5
+
+if(__name__) == '__main__':
+    n = 5
+    coord_1 = Coordenada(3, 30)
+    coord_2 = Coordenada(4, 8)
+
+    print(coord_1.distance(coord_2))
+    print(isinstance(coord_2, Coordenada))
+    print(isinstance(n, Coordenada))
---
+++ 2. OOP and Algorithms with Python/decorators.py
@@ -0,0 +1,61 @@
+from varname import nameof
+
+def decorator_function(funct):
+    def wrapper():
+        print('This is the last message')
+        funct()
+        print('This is the first message')
+    return wrapper
+
+def buzz1():
+    print('Buzzz')
+
+buzz = decorator_function(buzz1)
+
+buzz()
+
+#-----With better sintaxis-----
+def decorator_function2(funct):
+    def wrapper():
+        print('This is the last message')
+        funct()
+        print('This is the first message')
+    return wrapper
+
+@decorator_function2
+def buzz2():
+    print('Buzzz')
+
+buzz()
+
+#-----Understanding decorators-----
+def deco(funct):
+    print(f'Ejecutando el método {deco.__name__} con id {id(deco)}')
+    print(f'El ID de la función parámetro {funct.__name__} es: {id(funct)}')
+    def wrapper_func(x):
+        print(f'Ejecutando wrapper con la función {funct.__name__} como parámetro')
+        return funct(x)
+
+    print(f'El ID de la función {wrapper_func.__name__} es: {id(wrapper_func)}')
+    return wrapper_func
+
+def original_1(msg):
+    print(f'Ejecutando la función {original_1.__name__} con {msg}')
+
+print(f'El ID de la función {original_1.__name__} es: {id(original_1)}')
+
+original_1 = deco(original_1)
+
+print(f'El ID de la variable {nameof(original_1)} es: {id(original_1)}')
+
+def original_2(msg):
+    print(f'Ejecutando la función {original_2.__name__} con {msg}')
+
+print(f'El ID de la función {original_2.__name__} es: {id(original_2)}')
+
+original_2 = deco(original_2)
+
+print(f'El ID de la variable {nameof(original_2)} es: {id(original_2)}')
+
+original_1('MESSAGE')
+original_2('MESSAGE')
