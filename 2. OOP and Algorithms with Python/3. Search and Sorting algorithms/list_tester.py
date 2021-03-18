29972920ac1dbba6838d49d95cf0c11f9e23a6f3
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2021-03-18 04:26:32 -0500

    fixed list_tester

--- 2. OOP and Algorithms with Python/3. Search and Sorting algorithms/list_tester.py
+++ 2. OOP and Algorithms with Python/3. Search and Sorting algorithms/list_tester.py
@@ -8,21 +8,21 @@ def timer(funct):
     def wrapper_function(*Args, **Kwargs):
         time0 = time.time()
         funct(*Args, **Kwargs)
-        print(f'La función {funct.__name__} demoró {time.time() - time0} en correr')
+        print(f'La función demoró {time.time() - time0} en correr')
     return wrapper_function
 
 @timer
 def test_list(method, list):
     sorted_list = method(list)
     print(method.__name__)
-    if len(list) < 50:
+    if len(list) <= 50:
         print(f"Sorted list: ")
         print(sorted_list)
 
 def generate_list(list_size):
     list = [random.randint(0,list_size) for i in range(list_size)]
 
-    if len(list) < 50:
+    if len(list) <= 50:
         print("Original List: ")
         print(list)
 
@@ -41,18 +41,18 @@ def test_all_methods(list_size):
     list = generate_list(list_size)
 
     print("- " * 55)
-    test_list(bubble_sort.bubble_sort, list)
+    test_list(bubble_sort.bubble_sort, list.copy())
     print("- " * 25)
-    test_list(insertion_sort.insertion_sort, list)
+    test_list(insertion_sort.insertion_sort, list.copy())
     print("- " * 25)
-    test_list(merge_sort.merge_sort, list)
+    test_list(merge_sort.merge_sort, list.copy())
     print("- " * 25)
 
 if __name__ == '__main__':
     list_size = int(input('Write the desired list size: '))
 
     test_all_methods(list_size)
-    test_all_methods(list_size)
-    test_all_methods(list_size)
-    test_all_methods(list_size)
-    test_all_methods(list_size)
+    # test_all_methods(list_size)
+    # test_all_methods(list_size)
+    # test_all_methods(list_size)
+    # test_all_methods(list_size)
